34990a2b12441ad272fb21cefeae9063e9d61213
noear <noear@live.cn>
2025-08-24 00:39:59 +0800

    demo

--- solon-a2a-demo/src/test/java/com/a2a/demo/AgentTest.java
+++ solon-a2a-demo/src/test/java/com/a2a/demo/AgentTest.java
@@ -1,36 +1,29 @@
 package com.a2a.demo;
 
-import lombok.SneakyThrows;
-
 import org.junit.jupiter.api.Test;
+import org.noear.solon.ai.a2a.client.A2AAgent;
 import org.noear.solon.ai.chat.ChatModel;
 import org.noear.solon.ai.chat.ChatResponse;
-import org.noear.solon.annotation.Inject;
 import org.noear.solon.test.SolonTest;
 
-@SolonTest(value = ClientApp.class)
+@SolonTest
 public class AgentTest {
-    @Inject
-    HostAgent hostAgent;
 
-    @Inject
-    HostAgentTools hostAgentTools;
-
-    @SneakyThrows
     @Test
-    public void test1() {
+    public void test1() throws Exception {
+        A2AAgent agent = new A2AAgent();
+
         ChatModel chatModel = ChatModel.of("http://127.0.0.1:11434/api/chat")
                 .provider("ollama")
                 .model("qwen2.5:latest")
-                .defaultToolsAdd(hostAgentTools)
                 .build();
 
-        hostAgent.addChatModel(chatModel);
+        agent.addChatModel(chatModel);
 
-        hostAgent.register("http://localhost:10001");
-        hostAgent.register("http://localhost:10002");
+        agent.register("http://localhost:10001");
+        agent.register("http://localhost:10002");
 
-        ChatResponse chatResponse = hostAgent.chat("杭州今天的天气适合去哪里玩？");
+        ChatResponse chatResponse = agent.chatCall("杭州今天的天气适合去哪里玩？");
 
         System.err.println(chatResponse.getMessage().getResultContent());
     }
